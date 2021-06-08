a30e03f687afda14062aabcdbdf74565ad5b089a
Farzf19 <67421915+Farzanafrahman@users.noreply.github.com>
2021-06-08 20:17:51 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+//Contact class: Represents a Contact
+class Contact {
+    constructor(name, phNum, email) {
+        this.name = name;
+        this.phNum = phNum;
+        this.email = email;
+    }
+}
+
+//UI class: Handles UI Tasks
+
+
+//Store class: Handles storage
+
+
+//Event: Add a contact
+
+//Event Remove a contact
